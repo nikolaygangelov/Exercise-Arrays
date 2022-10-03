9f974d22522f50c13151d693e9c27b60023a2a49
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-03 23:18:01 +0300

    Add files via upload

---
+++ Zig-Zag Arrays.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace _3._Zig_Zag_Arrays
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] inputArray = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                if (i % 2 != 0)
+                {
+                    firstArray[i] = inputArray[0];
+                    secondArray[i] = inputArray[1];
+                }
+                else
+                {
+                    firstArray[i] = inputArray[1];
+                    secondArray[i] = inputArray[0];
+                }
+            }
+            Console.WriteLine(string.Join(' ', secondArray));
+            Console.WriteLine(string.Join(' ', firstArray));
+        }
+    }
+}
