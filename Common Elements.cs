69ae87719581157aac5db17886b9e49032ea5145
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-03 22:35:03 +0300

    Add files via upload

---
+++ Common Elements.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace _2._Common_Elements
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            string[] secondArr = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                for (int x = 0; x < firstArr.Length; x++)
+                {
+                    if (secondArr[i] == firstArr[x])
+                    {
+                        Console.Write($"{secondArr[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
