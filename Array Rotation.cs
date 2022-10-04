f970c841b33047f7721eaf8d124bcf694d085348
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-05 00:04:29 +0300

    Add files via upload

---
+++ Array Rotation.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _4._Array_Rotation
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    int first = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = first;
+                }
+                n--;
+            }
+            Console.WriteLine(string.Join(' ', arr)); 
+        }
+    }
+}
