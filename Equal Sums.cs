4fe5f2f9e03e4e4517b434072bba6273abb507d7
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-05 17:18:08 +0300

    Add files via upload

---
+++ Equal Sums.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace _6._Equal_Sums
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            bool isEqual = false;
+            for (int i = 0; i < arr.Length; i++)//1 2 3 3
+            {
+                for (int x = 0; x < arr.Length - 1 - i; x++)
+                {
+                    rightSum += arr[x +i + 1];
+                }
+                for (int x = i; x > 0; x--)
+                {
+                    leftSum += arr[x - 1];
+                }
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(i);
+                    isEqual = true;
+                    return;
+                }
+                rightSum = 0;
+                leftSum = 0;
+            }
+            if (isEqual)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            
+        }
+    }
+}
