2ff4fc74e25921bed1c6a3b28c1058f65e168388
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-06 20:55:28 +0300

    Add files via upload

---
+++ Magic Sum.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace _8._Magic_Sum
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int x = 0; x < arr.Length - 1 - i; x++)
+                {
+                    if (arr[i] + arr[i + x + 1] == magicNumber)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[i + x + 1]}");
+                    }
+                }
+            }
+        }
+    }
+}
